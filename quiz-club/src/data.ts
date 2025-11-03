7a07c4d6924fab11bca32ecf175cc7da393bd80a
Salar <SaliTech.2008@gmail.com>
2025-11-03 23:24:51 +0330

    Category Data

--- quiz-club/src/data.ts
+++ quiz-club/src/data.ts
@@ -5,24 +5,28 @@ export const quizQuestions: Question[] = [
     question: "what's my favorite food?",
     correct_answer: "pizza",
     incorrect_answers: ["chiken", "hot dog", "felafel"] ,
-    difficulty: "easy"
+    difficulty: "easy",
+    category: "computer_science"
     },
     {
     question: "Which planet is known as the Red Planet?",
     correct_answer: "Mars",
     incorrect_answers: ["Jupiter", "Saturn", "Venus"] ,
-    difficulty: "medium"
+    difficulty: "medium",
+    category: "math"
     },
     {
     question: "Which planet is known as the Red Planet?2",
     correct_answer: "Mars",
     incorrect_answers: ["Jupiter", "Saturn", "Venus"] ,
-    difficulty: "medium"
+    difficulty: "medium",
+    category: "history"
     },
     {
     question: "Which planet is known as the Red Planet?3",
     correct_answer: "Mars",
     incorrect_answers: ["Jupiter", "Saturn", "Venus"] ,
-    difficulty: "medium"
+    difficulty: "medium",
+    category: "math"
     },
 ]
--- quiz-club/src/types.ts
+++ quiz-club/src/types.ts
@@ -1,10 +1,12 @@
 type Difficulties = "hard" | "medium" | "easy";
+type Category = "sports" | "history" | "computer_science" | "math"
 
 export interface Question {
   question: string;
   correct_answer: string;
   incorrect_answers: string[];
-  difficulty: Difficulties
+  difficulty: Difficulties;
+  category: Category;
 }
 
 export interface QuestionState extends Question {
